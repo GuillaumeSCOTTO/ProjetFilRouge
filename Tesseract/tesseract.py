5fe82bf189dc0df44a6919e5f6e5b03f18a3999d
guillaume <guillaume.scotto@telecom-paris.fr>
2023-01-24 22:43:52 +0100

    Parcours dans tous les folders + élminiations de certaines images non compatibles

--- Tesseract/tesseract.py
+++ Tesseract/tesseract.py
@@ -18,11 +18,12 @@ def process_image2(image):
 def print_data(data): print(data)
 
 def output_file(f,texte_path,data):
-	path = texte_path + f+".txt"
+	path = texte_path + "texte.txt"
 	#print(path)
-	file = open(path, "w")
+	file = open(path, "a+")
 	#file = open(filename, "w+")
 	file.write(data)
+	file.write("\n \n")
 	file.close()
 
 
@@ -35,18 +36,25 @@ def preprocess_final(im):
 def main():
 	directory = './images'
 	textes_path = './textes/'
+	path = textes_path + "texte.txt"
+	file = open(path, "w")
+	file.close()
 	#textes_path2 = '/Users/scotto/PycharmProjects/ProjetFilRouge/textes2/'
-	for filename in os.listdir(directory):
-		f = os.path.join(directory, filename)
-		# checking if it is a file
-		if os.path.isfile(f):
-			# checking if it is an image
-			if imghdr.what(f):
-				im = np.array(Image.open(f))
-				im = preprocess_final(im)
-				text = process_image2(im)
-				#image = process_image(f, "eng")
-				output_file(filename,textes_path,text)
+	for root, dirs, files in os.walk("."):
+		path = root.split(os.sep)
+		print((len(path) - 1) * '---', os.path.basename(root))
+		for file in files:
+			print(len(path) * '---', file)
+			f = root+"/"+file
+			# checking if it is a file
+			if os.path.isfile(f):
+				# checking if it is an image
+				if imghdr.what(f):
+					im = np.array(Image.open(f))
+					im = preprocess_final(im)
+					text = process_image2(im)
+					#image = process_image(f, "eng")
+					output_file(f,textes_path,text)
 
 
 if  __name__ == '__main__':
